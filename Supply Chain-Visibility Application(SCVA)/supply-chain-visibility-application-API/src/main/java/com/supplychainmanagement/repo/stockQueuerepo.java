b56857bb2128d32854d70c29ce2e9fe285dfcdcf
JK <jupiterkingtechnologies@gmail.com>
2024-09-12 12:16:09 +0530

    Modified the Supply chain visibility project.

--- Supply Chain-Visibility Application(SCVA)/supply-chain-visibility-application-API/src/main/java/com/supplychainmanagement/repo/stockQueuerepo.java
+++
@@ -1,8 +0,0 @@
-package com.supplychainmanagement.repo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.supplychainmanagement.model.stockQueue;
-@Repository
-public interface stockQueuerepo extends JpaRepository<stockQueue, Long> {
-
-}
