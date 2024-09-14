2559af8bd23db402fd986ec228af474b9a6255dd
JK <jupiterkingtechnologies@gmail.com>
2024-09-14 11:17:28 +0530

    Modified the Suplly chain project.

--- Supply Chain-Visibility Application(SCVA)/supply-chain-visibility-application-API/src/main/java/com/supplychainmanagement/repo/StockKeepingUnitRepo.java
+++
@@ -1,8 +0,0 @@
-package com.supplychainmanagement.repo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.supplychainmanagement.model.StockKeepingUnit;
-@Repository
-public interface StockKeepingUnitRepo extends JpaRepository<StockKeepingUnit, Long> {
-
-}
--- Supply Chain-Visibility Application(SCVA)/supply-chain-visibility-application-API/src/main/java/com/supplychainmanagement/repo/supplierrepo.java
+++
@@ -1,9 +0,0 @@
-package com.supplychainmanagement.repo;
-
-import com.supplychainmanagement.model.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SupplierRepo extends JpaRepository<Supplier, Long> {
-}
